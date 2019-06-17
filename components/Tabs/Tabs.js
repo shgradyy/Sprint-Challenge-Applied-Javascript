1c8c1bfe891e50e282234425db2ebc2178c40988
Shantay Grady <shantaygrady@gmail.com>
2019-06-16 21:56:13 -0400

    Updates if statements and map

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,25 +4,25 @@ class TabLink {
      this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
-    this.tabData = document.querySelector('.tab').dataset.dataTab;
+    this.tabData = this.tabElement.dataset.dataTab;
 
     
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
-    //<- Delete this comment block when you work on the if statement
+    
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData == 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
       this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-       this.cards = document.querySelector("div[data-tab]");
+       this.cards = document.querySelector(".card[data-tab = 'this.tabData']");
     }
-    // <- Delete this comment block when you work on the if statement
+   
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map();
+    this.cards = Array.from(this.cards).map(tabElement => new TabCard(tabElement));
 
     // Add a click event that invokes this.selectTab
     // this.tabElement.addEventListener();
@@ -71,4 +71,4 @@ class TabCard {
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
 
 */
-let tabs = document.querySelectorAll('.tabs').forEach( tabLink => new TabLink(tabLink));
+let tabs = document.querySelectorAll('.tab').forEach( tabLink => new TabLink(tabLink));
