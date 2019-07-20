ea7b29ccaa02bae499747de7d0930a3c3e7b3871
Shantay Grady <shantaygrady@gmail.com>
2019-07-19 23:30:58 -0400

    Updates tabs component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,9 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const tabEl = document.querySelector('.topics');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+   .then(console.log(console))
+   .catch(console.log(console));
