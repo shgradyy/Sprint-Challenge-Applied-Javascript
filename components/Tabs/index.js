ba220337e6549ce238df6078355a846e46e6df46
Shantay Grady <shantaygrady@gmail.com>
2019-10-30 01:26:26 -0400

    adds datasets

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,11 +8,11 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
-let objRequest = axios
+const objRequest = axios
   .get("https://lambda-times-backend.herokuapp.com/topics")
   .then(response => {
-    response.data.topics.forEach(cv => {
-      return new TabEl(cv);
+    response.data.topics.forEach(currentValue => {
+      return new TabEl(currentValue);
     });
   })
   .catch(error => {
@@ -21,9 +21,28 @@ let objRequest = axios
 
 const topics = document.querySelector(".topics");
 
-function TabEl(obj) {
-  let div = document.createElement("div");
+const TabEl = (obj) => {
+  const div = document.createElement("div");
   div.classList.add("tab");
   div.textContent = obj;
   topics.appendChild(div);
 }
+window.setTimeout(() => {
+    const tabsList = document.querySelectorAll(".tab");
+    tabsList.forEach(currentValue => {
+      currentValue.dataset.tab = currentValue.textContent;
+      currentValue.addEventListener("click", () => {
+        tabsList.forEach(currentValue => {
+          currentValue.classList.remove("active-tab");
+        });
+        currentValue.classList.add("active-tab");
+        const cardsList = document.querySelectorAll(".card");
+        cardsList.forEach(av => {
+          av.style.display = "inline-block";
+          if (av.dataset.tab != currentValue.dataset.tab) {
+            av.style.display = "none";
+          }
+        });
+      });
+    });
+  }, 3000);
