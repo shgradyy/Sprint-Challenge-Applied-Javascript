d199a2d7c22c03f36f5e1bd49f2339e5b6cffe0b
Shantay Grady <shantaygrady@gmail.com>
2019-10-30 01:06:11 -0400

    Refactors Card element

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,8 +17,44 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
-axios.get('https://lambda-times-backend.herokuapp.com/articles')
-.then(dataSet => {
-    const articles = dataSet.data.articles;
-})
+const infoRequest = axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    Object.keys(response.data.articles).forEach(cv => {
+      return new cardEl(response.data.articles[cv]);
+    });
+  })
+  .catch(error => {
+    console.log("error", error);
+  });
+
+const cardsCont = document.querySelector(".cards-container");
+
+function cardEl(obj) {
+  obj.forEach(cv => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    cardsCont.appendChild(cardDiv);
+
+    const cardHeader = document.createElement("div");
+    cardHeader.classList.add("headline");
+    cardHeader.textContent = cv.headline;
+    cardDiv.appendChild(cardHeader);
+
+    const cardAuth = document.createElement("div");
+    cardAuth.classList.add("author");
+    cardDiv.appendChild(cardAuth);
+
+    const cardImgCont = document.createElement("div");
+    cardImgCont.classList.add("img-container");
+    cardAuth.appendChild(cardImgCont);
+
+    const cardImg = document.createElement("img");
+    cardImg.src = cv.authorPhoto;
+    cardImgCont.appendChild(cardImg);
+
+    const cardAuthName = document.createElement("span");
+    cardAuthName.textContent = `By: ${cv.authorName}`;
+    cardAuth.appendChild(cardAuthName);
+  });
 
